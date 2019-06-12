efb45a48fa2cdc30f1ec31e945571f172a02341e
PabloSz <pablosaez1995@gmail.com>
2019-06-11 20:26:59 -0400

    clean

--- dist/database.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const guardarJSON: (obj: {}, name: string, flag?: string) => Promise<void>;
-export declare const leerAPFile: () => Promise<{}>;
-export declare const guardarAPFile: (obj: {}) => Promise<{}>;
--- dist/wifi.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const getAccessPoints: () => Promise<any>;
-export declare const refreshAccessPoints: (networks: {
-    ssid: string;
-    mac: string;
-    channel: number;
-    signal_level: number;
-    quality: number;
-}[], node: number) => Promise<{}>;
